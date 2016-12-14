6f0ca8f794f4647716bfd8dfc3a27f8a31a5a351
Christopher62 <christopher.dachicourt@gmail.com>
2016-12-14 12:26:50 +0100

    Chall BootStrap

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function(){
+  $("#btn0").click(function(){
+      $("#cameleonDiv").animate( {backgroundColor:"red", color:"white"}, 1000 );
+  });
+
+  $("#btn1").dblclick(function(){
+      $("#cameleonDiv").animate( {backgroundColor:"green", color:"#FF9900"}, 1000 );
+  });
+
+  $("#btn2").click(function(){
+      $("#cameleonDiv").animate( {backgroundColor:"#99CCFF", color:"black"}, 1000 );
+  });
+  $("#btn3").click(function(){
+      $("#contain, #portfolio").animate( {backgroundColor:"#FF9900", color:"black"}, 5000 );
+      $("#contain, #portfolio").animate( {backgroundColor:"#FF9900", color:"black"}, 1000 );
+  });
+});
+$(document).ready(function(){
+    $("#disappear").click(function(){
+        $(this).hide();
+    });
+});
+    $("#iconup").click(function(){
+        alert("Voulez-vous retourner en haut de page ?");
+        alert("Vous ne voulez pas retourner sur une partie précise ?");
+        alert("Bon ben retour en haut de page !");
+    });
